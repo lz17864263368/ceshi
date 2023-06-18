caefc35104aaecc5adef128e2e3395b47e46e81e
lz17864263368qd <17864263368@163.com>
2023-06-18 09:51:08 +0800

    first init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>子组件内容：{{childdata}}{{msg}}</p>
+    <p>
+      <button @click="changefather">调用父组件内容</button>
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps,defineExpose,defineEmits,ref,toRefs} from "vue";
+//1.父组件传值接收
+const props = defineProps({
+  //子组件接收父组件传递过来的值
+  msg: String,
+});
+const { msg } = toRefs(props);
+let childdata=ref('12345')
+// 调用父组件的方法
+const emit = defineEmits(["refsh"]);
+// emit('refsh')
+const changefather=()=>{
+   emit('refsh')
+}
+defineExpose({
+  childdata,
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <button @click="btnclick">点击</button>
+    {{frvalue}}
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App"  @refsh='changefr' ref="childref"/>
+  </div>
+</template>
+
+<script setup>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import {ref} from 'vue'
+const childref=ref('')
+
+const btnclick=()=>{
+  console.log('打印了',childref.value.childdata)
+  childref.value.childdata='456789'//根据ref更改子组件变量或者调用子组件方法
+  console.log('打印了',childref.value.childdata)
+  
+}
+//设置子组件要改变的值
+let frvalue=ref('')
+const changefr=()=>{
+  frvalue.value='子组件触发了父组件的方法'
+}
+</script>
